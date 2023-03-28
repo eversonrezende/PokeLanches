d92e894f5efcb25bcfa6ebb7cf51e730a1aecca2
erezendeosg <erezende@osg.com.br>
2023-03-28 12:27:41 -0300

    cria projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokeLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
