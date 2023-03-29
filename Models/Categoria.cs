716271cc5a6f6a9a86628e1b5d22d9f1fc3391c5
erezendeosg <erezende@osg.com.br>
2023-03-29 13:17:55 -0300

    aplica migration lanches e categorias

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -8,13 +8,17 @@ public class Categoria
 {
     [Key]
     public int CategoriaId { get; set; }
+
     [StringLength(100, ErrorMessage ="O tamanho máximo é de 100 caracteres")]
     [Required(ErrorMessage ="Informe o nome da categoria")]
     [Display(Name ="Nome")]
     public string CategoriaNome { get; set; }
+
     [StringLength(200, ErrorMessage = "O tamanho máximo é de 200 caracteres")]
     [Required(ErrorMessage = "Informe o descrição da categoria")]
+    [Display(Name ="Descrição")]
     public string Descricao { get; set; }
+
     public List<Lanche> Lanches { get; set;} 
 }
 
