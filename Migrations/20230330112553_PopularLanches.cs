74dd360ea9f95b04cc5460945e06ff267162df31
erezendeosg <erezende@osg.com.br>
2023-03-30 11:15:29 -0300

    popula tabela lanches

---
+++ Migrations/20230330112553_PopularLanches.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeLanches.Migrations
+{
+    public partial class PopularLanches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1,'Eevee Latte','A signature latte with a comforting flavor. Enjoy the cute latte art of Eevee!',1, 'https://archives.bulbagarden.net/media/upload/0/01/Caf%C3%A9_ReMix_Eevee_Latte.png','https://archives.bulbagarden.net/media/upload/0/01/Caf%C3%A9_ReMix_Eevee_Latte.png', 0, 'Eevee Latte', 8.00)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
---
+++ Migrations/20230330134746_PopularLanchesV2.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeLanches.Migrations
+{
+    public partial class PopularLanchesV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1,'Lilligant Floral Tea','Original blended tea. The soothing scent has calming benefits, just like the flower on Lilligants head.',1, 'https://archives.bulbagarden.net/media/upload/a/aa/Caf%C3%A9_ReMix_Lilligant_Floral_Tea.png','https://archives.bulbagarden.net/media/upload/a/aa/Caf%C3%A9_ReMix_Lilligant_Floral_Tea.png', 0, 'Lilligant Floral Tea', 11.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1,'Fluffy Eevee Pancakes','The whipped cream (as fluffy as Eevees fur!) is a huge hit. Pure delight until the very last bite!',1,'https://archives.bulbagarden.net/media/upload/c/c6/Caf%C3%A9_ReMix_Fluffy_Eevee_Pancakes.png','https://archives.bulbagarden.net/media/upload/c/c6/Caf%C3%A9_ReMix_Fluffy_Eevee_Pancakes.png', 0, 'Fluffy Eevee Pancakes', 12.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(2,'Dugtrio Sandwich Trio','A signature latte with a comforting flavor. Enjoy the cute latte art of Eevee!',1,'https://archives.bulbagarden.net/media/upload/f/f8/Caf%C3%A9_ReMix_Dugtrio_Sandwich_Trio.png','https://archives.bulbagarden.net/media/upload/f/f8/Caf%C3%A9_ReMix_Dugtrio_Sandwich_Trio.png', 1, 'Dugtrio Sandwich Trio', 7.50)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
