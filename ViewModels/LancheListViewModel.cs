1eeddc62a87b54a32627c62482229885a1a609e9
erezendeosg <erezende@osg.com.br>
2023-04-06 14:17:52 -0300

    implementa ViewModel LancheCategoria

---
+++ ViewModels/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PokeLanches.Models;
+
+namespace PokeLanches.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
