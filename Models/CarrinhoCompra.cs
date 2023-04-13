a79aebd5fbf18bb194c90870cca4281b3ff808f0
erezendeosg <erezende@osg.com.br>
2023-04-12 22:28:12 -0300

    cria CarrinhoCompra

---
+++ Models/CarrinhoCompra.cs
@@ -0,0 +1,39 @@
+﻿using PokeLanches.Context;
+
+namespace PokeLanches.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //define uma sessão
+            //operador Elvis
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um servico do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            //obtem ou gera o Id do carrinho
+            //operador de calissencia nula
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //atribui o id do carrinho na Sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+    }
+}
